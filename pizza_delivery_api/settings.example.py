fa2b02762f9cb3a7b1e5931a1dfe1d66580c2b8c
DagiiM <douglasmutethia2017@gmail.com>
2023-02-17 16:05:12 +0300

    settings update

--- pizza_delivery_api/settings.example.py
+++ pizza_delivery_api/settings.example.py
@@ -15,11 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)n5r#bt0uldlm$_%%*sf1zr5oomwk31pfn5en0pr3aln=60@+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
